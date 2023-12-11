546c41984005fc90363603ec1ba10d6e8531b4cb
Telman Yusifov <143818063+CSRept1l1an@users.noreply.github.com>
2023-12-11 09:50:46 +0400

    Update XSS.py

--- XSS.py
+++ XSS.py
@@ -5,9 +5,7 @@ def detect_xss(input_text):
 
     if xss_pattern.search(input_text):
         print(f'Potential XSS attack detected: {input_text}')
+        return True
     else:
         print("No Potential XSS attack detected")
-     
-#Example for XSS attack
-user_input = '<p><script>alert("Hello, XSS!");</script></p>'
-detect_xss(user_input)
+        return False
